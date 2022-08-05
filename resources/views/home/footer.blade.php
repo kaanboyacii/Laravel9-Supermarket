3130817bde45a16677a84d2f6f0fb3f3a6e257b8
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-08-05 09:31:42 +0300

    Footer links, routes

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -18,31 +18,23 @@
                     <div class="footer__widget">
                         <h6>Useful Links</h6>
                         <ul>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">About Our Shop</a></li>
-                            <li><a href="#">Secure Shopping</a></li>
-                            <li><a href="#">Delivery infomation</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Our Sitemap</a></li>
+                            <li><a href="{{route('home')}}">Anasayfa</a></li>
+                            <li><a href="{{route('about')}}">Hakkımızda</a></li>
+                            <li><a href="{{route('contact')}}">İletişim</a></li>
+                            <li><a href="{{route('faq')}}">Sıkça Sorulan Sorular</a></li>
+                            <li><a href="#">Gizlilik İlkeleri</a></li>
                         </ul>
                         <ul>
-                            <li><a href="#">Who We Are</a></li>
-                            <li><a href="#">Our Services</a></li>
-                            <li><a href="#">Projects</a></li>
-                            <li><a href="#">Contact</a></li>
-                            <li><a href="#">Innovation</a></li>
-                            <li><a href="#">Testimonials</a></li>
+                            <li><a href="/loginuser">Üye Giriş</a></li>
+                            <li><a href="/registeruser">Üye Ol</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-12">
                     <div class="footer__widget">
-                        <h6>Join Our Newsletter Now</h6>
-                        <p>Get E-mail updates about our latest shop and special offers.</p>
-                        <form action="#">
-                            <input type="text" placeholder="Enter your mail">
-                            <button type="submit" class="site-btn">Subscribe</button>
-                        </form>
+                        <h6>Hemen Üye Ol</h6>
+                        <a class="site-btn" href="/registeruser">Üye Ol</a>
+                        <hr>
                         <div class="footer__widget__social">
                             <a href="{{$setting->facebook}}"><i class="fa fa-facebook"></i></a>
                             <a href="{{$setting->instagram}}"><i class="fa fa-instagram"></i></a>
@@ -57,11 +49,9 @@
                     <div class="footer__copyright">
                         <div class="footer__copyright__text">
                             <p>
-                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                                 Copyright &copy;<script>
                                     document.write(new Date().getFullYear());
-                                </script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://github.com/kaanboyacii" target="_blank">Kaan Boyacı</a>
-                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                </script> İz Market A.Ş | Bu İnternet Sitesi <a href="https://github.com/kaanboyacii" target="_blank">Kaan Boyacı</a> Tarafından Yapıldı.
                             </p>
                         </div>
                         <div class="footer__copyright__payment"><img src="img/payment-item.png" alt=""></div>
